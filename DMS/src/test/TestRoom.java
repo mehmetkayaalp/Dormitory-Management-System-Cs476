7d99dc073f079a947c7c2f3e0e615ce47199a060
Erdi <erdi.koc@ozu.edu.tr>
2015-03-18 17:14:52 +0200

    Update TestRoom.java

--- DMS/src/test/TestRoom.java
+++ DMS/src/test/TestRoom.java
@@ -16,12 +16,45 @@ public class TestRoom {
 	}
 	
 	@Test
+	public void testTypeName2() {
+		Room room = new Room();
+		room.setTypeName("");
+		assertEquals("", room.getTypeName());
+	}
+	
+	@Test
+	public void testTypeName3() {
+		Room room = new Room();
+		room.setTypeName("asdfghj");
+		assertEquals("asdfghj", room.getTypeName());
+	}
+	
+	@Test
 	public void testRoomNo() {
 		Room room = new Room();
 		room.setRoomNo(0);
 		assertEquals(0, room.getRoomNo());
 	}
 	
+	@Test
+	public void testRoomNo2() {
+		Room room = new Room();
+		room.setRoomNo(10);
+		assertEquals(10, room.getRoomNo());
+	}
+	
+	@Test
+	public void testRoomNo3() {
+		Room room = new Room();
+		room.setRoomNo(100);
+		assertEquals(100, room.getRoomNo());
+	}
+	@Test
+	public void testRoomNo4() {
+		Room room = new Room();
+		room.setRoomNo(1000);
+		assertEquals(1000, room.getRoomNo());
+	}
 	@SuppressWarnings("deprecation")
 	@Test
 	public void testRoomPrice() {
