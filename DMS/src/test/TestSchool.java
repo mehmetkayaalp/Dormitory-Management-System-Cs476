1de5cdaba17de1ed02fa98c251bdeea9cbc3f63c
Erdi <erdi.koc@ozu.edu.tr>
2015-03-18 18:15:40 +0200

    Test School
    School test added !

---
+++ DMS/src/test/TestSchool.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import background.School;
+
+public class TestSchool {
+
+	@Test
+	public void testUniversityName() {
+		School school = new School();
+		school.setUniName("Ozyeğin University");
+		assertEquals("Ozyeğin University",school.getUniName());
+	}
+	
+	@Test
+	public void testDepartment() {
+		School school = new School();
+		school.setDepartment("Computer Science");
+		assertEquals("Computer Science",school.getDepartment());
+	}
+	
+	@Test
+	public void testGrade() {
+		School school = new School();
+		school.setGrade(3);
+		assertEquals(3,school.getGrade());
+	}
+
+}
