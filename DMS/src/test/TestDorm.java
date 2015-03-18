99f45f0a85f942a476c6fb304f74e945f545781b
Erdi <erdi.koc@ozu.edu.tr>
2015-03-18 17:52:34 +0200

    Dorm Test
    Dorm Test Added !

---
+++ DMS/src/test/TestDorm.java
@@ -0,0 +1,46 @@
+package test;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import background.Dorm;
+
+public class TestDorm {
+	
+	@Test
+	public void testDormName() {
+		Dorm dorm = new Dorm();
+		dorm.setDormName("Nazlı's Home");
+		assertEquals("Nazlı's Home", dorm.getDormName());
+	}
+	
+	@Test
+	public void testDormName2() {
+		Dorm dorm = new Dorm();
+		dorm.setDormName("");
+		assertEquals("", dorm.getDormName());
+	}
+	
+	@Test
+	public void testDormName3() {
+		Dorm dorm = new Dorm();
+		dorm.setDormName(" ");
+		assertEquals(" ", dorm.getDormName());
+	}
+	
+	@Test
+	public void testDormLocation() {
+		Dorm dorm = new Dorm();
+		dorm.setLocation("Cekmekoy");
+		assertEquals("Cekmekoy", dorm.getLocation());
+	}
+	
+	@Test
+	public void testDormLocationLength() {
+		equals(new Dorm().getLocation().length() < 100 );
+	}
+
+}
